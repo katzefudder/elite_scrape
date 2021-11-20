8b98c6bdd7f19d8ffe7ed3a629fccdbb33f3a1c4
Florian Dehn <flo@katzefudder.de>
2021-11-20 12:37:03 +0100

    scrape team stripes

---
+++ elitescrape/elitescrape/spiders/team_stripes.py
@@ -0,0 +1,47 @@
+import scrapy, re
+from scrapy.http.request import Request
+
+class EliteSpider(scrapy.Spider):
+  name = "elite"
+
+  content = ""
+
+  teams = {
+    'str' : 'https://www.del-2.org/teamstripes/'
+  }
+
+  def parse(self, response):
+    # invert the dict
+    team_keys = dict(zip(self.teams.values(), self.teams.keys()))
+
+    page = response.url.split('/')[-1]
+    filename = '%s.txt' % page
+
+    # get the team's name
+    team = str(response.css('#wrapper_border #subcontent h1::text').get()).strip()
+
+    # set a title
+    content = "\n\n- " + str(team) + " -\n\n"
+
+    for players in response.xpath('//*[@id="tabelle"]//tbody/tr'):
+      current_team_key = team_keys[response.url]
+      
+      number = players.xpath('td[1]//text()').extract_first()
+      prename = players.xpath('td[3]//text()').extract_first()
+      name = players.xpath('td[2]//text()').extract_first()
+      content += "%s%s\t-%s- %s %s (%s)\n" % (current_team_key, number, number, prename, name, team)
+
+    self.content += content
+
+  # write content to a file
+  def writeFile(self, content):
+    with open('team_stripes.txt', 'w+') as f:
+      f.write(content)
+
+  # override start_request to use an own dict instead of start_urls
+  def start_requests(self):
+    for key, url in self.teams.items():
+      yield Request(url, self.parse)
+
+  def __del__(self):
+    self.writeFile(self.content)
