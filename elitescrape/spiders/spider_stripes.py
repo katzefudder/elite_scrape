e52143da755bca37e9664ddc779f6c611655005e
Florian Dehn <flo@katzefudder.de>
2023-10-02 15:29:12 +0200

    fix teamstripes

--- elitescrape/spiders/spider_stripes.py
+++ elitescrape/spiders/spider_stripes.py
@@ -13,17 +13,25 @@ class StripesSpider(EliteSpider):
     team_keys = dict(zip(self.teams.values(), self.teams.keys()))
 
     # get the team's name
-    team = str(response.css('#wrapper_border #subcontent h1::text').get()).strip()
+    team = str(response.xpath('//*[@id="ip_page_wrapper"]//div//section//div//div//h1//text()').get()).strip()
 
     # set a title
     content = "- " + str(team) + " -\n\n"
 
-    for players in response.xpath('//*[@id="tabelle"]//tbody/tr'):
+    for referees in response.xpath('//*[@id="ip_page_wrapper"]//div//section[2]//div[2]//div[@class="col"]'):
       current_team_key = team_keys[response.url]
       
-      number = players.xpath('td[1]//text()').get().strip()
-      prename = players.xpath('td[4]//text()').get().strip()
-      name = players.xpath('td[3]//b//text()').get().strip()
-      content += "%s%s\t-%s- %s %s (%s)\n" % (current_team_key, number, number, prename, name, team)
+      number = referees.xpath('div//div[2]/span//text()').get().strip().replace('#', '')
+      prename = referees.xpath('div//div[2]/h3//text()').get().strip()
+      name = referees.xpath('div//div[2]/h3//b//text()').get().strip()
+      content += "%s%s\t-%s- %s %s (Hauptschiedsrichter %s)\n" % (current_team_key, number, number, prename, name, team)
+
+    for referees in response.xpath('//*[@id="ip_page_wrapper"]//div//section[3]//div[2]//div[@class="col"]'):
+      current_team_key = team_keys[response.url]
+      
+      number = referees.xpath('div//div[2]/span//text()').get().strip().replace('#', '')
+      prename = referees.xpath('div//div[2]/h3//text()').get().strip()
+      name = referees.xpath('div//div[2]/h3//b//text()').get().strip()
+      content += "%s%s\t-%s- %s %s (Linesperson %s)\n" % (current_team_key, number, number, prename, name, team)
 
     self.results[team_keys[response.url]] = content
